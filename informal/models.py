eeae0cd7cf5c1e6deb703c62718ef9775edc4588
econwalter23 <econ.wruelasw23@gmail.com>
2020-12-14 10:02:26 -0500

    commit2

--- informal/models.py
+++ informal/models.py
@@ -19,50 +19,67 @@ empresas del sector formal al informal
 """
 
 class Constants(BaseConstants):
+
     name_in_url = 'Informal Game'
-    players_per_session = 20
-    num_rounds = 10
-    media_A = 40
-    media_B = 70
-    media_mayor = 0.16
+    players_per_group = 2
+    num_rounds = 5
+    endow_B = c(70)
+    endow_A = c(endow_B*0.25)
+    s_minB = -5
+    s_maxB = 5
+    s_minA = -3
+    s_maxA = 3
+    p_like = 0.5
+    q_like = 0.5
+
     impuesto = 0.18
+
+    e_payment_ntA_ntB = endow_A + impuesto*(endow_B + (s_minB+s_maxB)*0.5)
+    e_payment_tA = (1-impuesto)*(endow_A + (s_minA + s_maxA)*0.5)
+
+    e_payment_ntB = (1-impuesto)*(endow_B + (s_minB + s_maxB)*0.5)
+    e_payment_tB_tA = endow_B + impuesto*(endow_A + (s_minA + s_maxA)*0.5)
+
+
     instructions_template = 'informal/instructions.html'
-    rho = 0.7
-
-        
+    instructions_template_A = 'informal/instructionsA.html'
     
-
-
-
 
 class Subsession(BaseSubsession):
-    
-     def creating_session(self):
-        for p in self.get_players():
-            
-            p.asignar_grupo = random.randint(1, Constants.players_per_session+1)
-    
-
+    pass
 
 class Group(BaseGroup):
-    decision_A = models.BooleanField(label='¿ Le gustaría continuar en su grupo actual o cambiar al grupo B?'.
-                                    choices=[ 
-                                        [True,'Continuar']
-                                        [False, 'Cambiar']
-                                    ]
-                                    )
-    
-    decision_B = models.BooleanField(label='¿ Le gustaría continuar en su grupo actual o cambiar al grupo A?'.
-                                    choices=[ 
-                                        [True,'Continuar']
-                                        [False, 'Cambiar']
-                                    ]
-                                    )
+
+    def set_payoffs(self):
+        p1 = self.get_player_by_id(1)
+        p2 = self.get_player_by_id(2)
+        
+        p1.prod = random.uniform(Constants.s_minA,Constants.s_maxA)
+        p2.prod = random.uniform(Constants.s_minB,Constants.s_maxB)    
+
+        if p1.decision == 'Transitar':
+            if p2.decision == 'Transitar':
+                p1.payoff = (1-Constants.impuesto)*(Constants.endow_A + p1.prod)
+                p2.payoff = Constants.endow_B + Constants.impuesto*(Constants.endow_A + p1.prod)
+            
+            else:
+                
+                p1.payoff = (1-Constants.impuesto)*(Constants.endow_A + p1.prod)
+                p2.payoff = (1-Constants.impuesto)*(Constants.endow_B + p2.prod)
+        else:    
+            if p2.decision == 'Transitar':
+                p1.payoff = Constants.endow_A
+                p2.payoff = Constants.endow_B
+            else:
+                p1.payoff = Constants.endow_A + Constants.impuesto*(Constants.endow_B + p2.prod)
+                p2.payoff = (1-Constants.impuesto)*(Constants.endow_B + p2.prod)
 
 class Player(BasePlayer):
     
-    def role(self):
-        if self.asignar_grupo <= (0,60)*Constants.players_per_session:
-            return 'grupo A'
-        else:
-            return 'grupo B'
+    decision = models.BooleanField(label='¿ Le gustaría continuar en su grupo actual o transitar al otro grupo?',
+                                    choices=[ 
+                                        [True,'Transitar'],
+                                        [False, 'No transitar']
+                                    ]
+                                    )
+    
--- informal/pages.py
+++ informal/pages.py
@@ -1,82 +1,36 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
-import random
+
+#import random
 
 class Introduction(Page):
+    
     def is_displayed(self):
         return self.round_number == 1
 
-class MyPageB(Page):
-    
-    def before_next_page(self):
-        for p in self.group.get_players():
-            if p.role() == 'grupo B':
-                if self.round_number == 1: 
-                    p.product = random.randint(-10,10)
-                    if p.product > 0:
-                        p.transfer_B = (Constants.media_B + p.product)*Constants.impuesto
-                        p.endowment_B = (Constants.media_B + p.product)*(1 - Constants.impuesto)
-                    else:
-                        p.transfer_B = 0 
-                        p.endowment_B = Constants.media_B + p.product
-                else:
-                    p.product = Constants.rho*p.product + random.randint(-10,10)
-                    if p.product > 0:
-                        p.transfer_B = (p.endowment_B + p.product)*(Constants.impuesto)
-                        p.endowment_B = (p.endowment_B + p.product)*(1 - Constants.impuesto)
-                    else:
-                        p.transfer_B = 0
-                        p.endowment_B = p.endowment_B + (1 + Constants.rho)*p.product
-            pass   
+class MyPage(Page):
+
+    form_model ='player'
+    form_fields = ['decision']
     
     def is_displayed(self):
-        return self.p.role() == 'grupo B' 
+        return self.round_number == 1
 
-class MyPageB2(Page):
-    
-    form_model ='group'
-    form_fields = ['decision_B']
-    
-    def is_displayed(self):
-        return self.p.role() == 'grupo B' 
+class MyPage2(Page):
 
-class WaitForB(WaitPage):
+    form_model ='player'
+    form_fields = ['decision']
     
     def is_displayed(self):
-        return self.p.role() == 'grupo A' 
-
-class MyPageA(Page):
+        return self.round_number > 1    
     
-    def before_next_page(self):
-        for p in self.group.get_players():
-            if p.role() == 'grupo A':
-                if self.round_number == 1: 
-                    p.transfer_A = p.transfer_B
-                    p.endowment_A = Constants.media_A + p.transfer_A
-                else:
-                    p.endowment_A = p.endowment_A + p.transfer_A
-            pass
-
-    def is_displayed(self):
-        return self.p.role() == 'grupo A'
-
-class MyPageA2(Page):
-    
-    form_model ='group'
-    form_fields = ['decision_A']
-    
-    def is_displayed(self):
-        return self.p.role() == 'grupo A' 
-
-
-
 class ResultsWaitPage(WaitPage):
-    pass
-
+    after_all_players_arrive = 'set_payoffs'
 
 class Results(Page):
     pass
+        
 
 
-page_sequence = [Introduction, MyPageB, MyPageA, MyPageB2, ResultsWaitPage, Results]
+page_sequence = [Introduction, MyPage, MyPage2, ResultsWaitPage, Results]
